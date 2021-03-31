d58b9a7f5e563afb5030a44dd557264d1f5ea078
Adam Marciszewski <amarciszewski@ithaca.edu>
2021-03-30 22:29:00 -0400

    created Player class, not implemented yet

---
+++ src/main/java/edu/ithaca/dragon/bank/Player.java
@@ -0,0 +1,23 @@
+import jdk.internal.net.http.common.Pair;
+
+public class Player {
+    private String name;
+    private String ID;
+    private double[] schedule;
+
+    public Player() {
+        ;
+    }
+
+    public String getName() {
+        ;
+    }
+
+    public String getID() {
+        ;
+    }
+
+    public double[] getSchedule() {
+        ;
+    }
+}
