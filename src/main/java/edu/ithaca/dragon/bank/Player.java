e5ecad1a3136cf136a354f6aff7715572fa6d846
Adam Marciszewski <amarciszewski@ithaca.edu>
2021-03-31 09:14:33 -0400

    added doc to player

--- src/main/java/edu/ithaca/dragon/bank/Player.java
+++ src/main/java/edu/ithaca/dragon/bank/Player.java
@@ -3,10 +3,22 @@ import jdk.internal.net.http.common.Pair;
 public class Player {
     private String name;
     private String ID;
-    private double[] schedule;
+    private Pair<Double, Double> schedule;
 
-    public Player() {
+    /**
+     * Constructor for the Player class
+     * @param name must be a name consisting of at least 1 character, number, or symbol
+     * @param ID must be a string of 4 numbers
+     * @param startTime must be a double greater than 0 and less than 24, using increments of 0.25 for 15 minute blocks of time
+     * @param endTime must be a double greater than 0 and less than 24, using increments of 0.25 for 15 minute blocks of time
+     * @throws IllegalArgumentException for any arguments that violate the above rules
+     */
+    public Player(String name, String ID, double startTime, double endTime) throws IllegalArgumentException {
         ;
+    }
+
+    public static boolean validateArguments(String name, String ID, double startTime, double endTime) {
+        ;   
     }
 
     public String getName() {
@@ -17,7 +29,7 @@ public class Player {
         ;
     }
 
-    public double[] getSchedule() {
+    public Pair<Double, Double> getSchedule() {
         ;
     }
 }
