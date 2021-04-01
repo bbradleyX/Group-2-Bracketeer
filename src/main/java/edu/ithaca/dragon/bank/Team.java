fb6486f27df0004cd662abeb88eadcc34e7d1b8c
Bobolicious cool kid <bbradley@ithaca.edu>
2021-04-01 15:03:28 -0400

    merging with MergeProcess

--- src/main/java/edu/ithaca/dragon/bank/Team.java
+++ src/main/java/edu/ithaca/dragon/bank/Team.java
@@ -1,6 +1,6 @@
 package edu.ithaca.dragon.bank;
 import java.util.ArrayList;
-import java.util.List;
+//import java.util.List;
 
 import org.javatuples.Pair;
 
@@ -8,8 +8,8 @@ import org.javatuples.Pair;
 
 public class Team {
     private String teamName;
-    private List <Pair<Double, Double>> teamSchedule;
-    private List <Player> teamPlayers;
+    private ArrayList <Pair<Double, Double>> teamSchedule;
+    private ArrayList <Player> teamPlayers;
 
     public Team(String teamName){
         this.teamName = teamName;
@@ -17,17 +17,39 @@ public class Team {
         this.teamSchedule = new ArrayList<Pair<Double, Double>>();
 
     }
+    private void addPlayer(Player player){
+        if (teamPlayers.isEmpty()){
+            teamPlayers.add(player);
+        }
+        else{
+            teamPlayers.add(player);
+
+        }
+    }
+
+    private void removePlayer(Player player) throws IndexOutOfBoundsException{
+        if (teamPlayers.isEmpty()){
+            throw new IndexOutOfBoundsException("This team list is empty. There are no players in this team.");
+        }
+        else{
+            teamPlayers.add(player);
+        }
+    }
 
     public String getTeamName(){
         return teamName;
     }
 
-    public List  <Player> getTeam(){
+    public ArrayList <Player> getTeam(){
         return teamPlayers;
     }
 
-    public List <Pair<Double, Double>> getTeamSchedule(){
+    public ArrayList <Pair<Double, Double>> getTeamSchedule(){
         return teamSchedule;
+    }
+
+    public Pair<Double, Double> viewSchedule (Player player){
+        return player.getSchedule();
     }
 
 
--- src/test/java/edu/ithaca/dragon/bank/TeamTest.java
+++ src/test/java/edu/ithaca/dragon/bank/TeamTest.java
@@ -11,7 +11,31 @@ public class TeamTest {
 
     @Test
     void createTeamsTest(){
-        
+        //Tests for 
+        Team team1 = new Team ("Team 1");
+        Team team2 = new Team ("Team2");
+        Team team3 = new Team ("");
+        Team team4 = new Team (" ");
+
+        //Unit Test for team names
+        assertEquals("Team 1", team1.getTeamName());
+        assertEquals("Team2", team2.getTeamName());
+        assertTrue(team3.getTeamName().contains("Invalid Team Name"));
+        assertTrue(team4.getTeamName().contains("Invalid Team Name"));
+
+        //Unit Test for team player lists
+        assertEquals([], team1.getTeam());
+
+        //Unit Test for team player schedule
+
     }
+
+    void addAndRemoveTeamsTest(){}
+
+    void getTeamNameTest(){}
+
+    void getTeamMembersTest(){}
+
+    void getTeamScheduleTest(){}
     
 }
