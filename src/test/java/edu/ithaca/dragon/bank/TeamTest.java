ee66269adc65e473ebc56d2e635cd7b755bb5881
Bobolicious cool kid <bbradley@ithaca.edu>
2021-04-01 15:10:27 -0400

    tests

--- src/test/java/edu/ithaca/dragon/bank/TeamTest.java
+++ src/test/java/edu/ithaca/dragon/bank/TeamTest.java
@@ -3,15 +3,12 @@ package edu.ithaca.dragon.bank;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 
 public class TeamTest {
-
     @Test
     void createTeamsTest(){
-        //Tests for 
         Team team1 = new Team ("Team 1");
         Team team2 = new Team ("Team2");
         Team team3 = new Team ("");
@@ -20,11 +17,8 @@ public class TeamTest {
         //Unit Test for team names
         assertEquals("Team 1", team1.getTeamName());
         assertEquals("Team2", team2.getTeamName());
-        assertTrue(team3.getTeamName().contains("Invalid Team Name"));
+        assertEquals("Index: 1, Size: 0", thrown.getMessage());
         assertTrue(team4.getTeamName().contains("Invalid Team Name"));
-
-        //Unit Test for team player lists
-        assertEquals([], team1.getTeam());
 
         //Unit Test for team player schedule
 
