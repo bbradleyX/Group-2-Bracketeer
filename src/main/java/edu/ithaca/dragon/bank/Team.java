41b954499b7bc4100b0c96f7f18b2ef8af828e38
Bobolicious cool kid <bbradley@ithaca.edu>
2021-04-16 11:34:46 -0400

    test run

--- src/main/java/edu/ithaca/dragon/bank/Team.java
+++ src/main/java/edu/ithaca/dragon/bank/Team.java
@@ -1,6 +1,10 @@
 package edu.ithaca.dragon.bank;
-import java.util.ArrayList;
+import java.security.InvalidParameterException;
 //import java.util.List;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import org.javatuples.Pair;
 
@@ -8,50 +12,93 @@ import org.javatuples.Pair;
 
 public class Team {
     private String teamName;
-    private ArrayList <Pair<Double, Double>> teamSchedule;
-    private ArrayList <Player> teamPlayers;
+    private HashMap<Player, Pair<Double, Double>> teamsMap;
 
     public Team(String teamName){
         this.teamName = teamName;
-        this.teamPlayers = new ArrayList<Player>();
-        this.teamSchedule = new ArrayList<Pair<Double, Double>>();
+        this.teamsMap = new HashMap<Player, Pair<Double, Double>>();
 
     }
-    public void addPlayer(Player player){
-        if (teamPlayers.isEmpty()){
-            teamPlayers.add(player);
+
+    public void addPlayer(Player player)throws InvalidParameterException{
+        if(teamsMap.isEmpty()){
+            System.out.println("Adding first player to "+teamName+ "...");
+            teamsMap.put(player, player.getSchedule());
         }
+        else if(!teamsMap.isEmpty()){
+            for ( Player key : teamsMap.keySet() ) {
+                if (player==key){
+                   throw new InvalidParameterException("Player is already in team: "+teamName);
+                }
+            }
+         }
         else{
-            teamPlayers.add(player);
+            System.out.println("Adding new player to "+teamName+ "...");
+            teamsMap.put(player, player.getSchedule());;
 
         }
     }
 
-    public void removePlayer(Player player) throws IndexOutOfBoundsException{
-        if (teamPlayers.isEmpty()){
+    public void removePlayer(String playerID) throws IndexOutOfBoundsException, IllegalArgumentException{
+        if (teamsMap.isEmpty()){
             throw new IndexOutOfBoundsException("This team list is empty. There are no players in this team.");
         }
         else{
-            teamPlayers.add(player);
+            for ( Player key : teamsMap.keySet() ) {
+                Player player = key;
+                if (player.getID() == playerID){
+                    teamsMap.remove(key);
+                }
+                else{
+                    throw new IllegalArgumentException("Player Not Found");
+                }
+            }
         }
     }
+
+    public boolean getPlayerInfo(String playerID)throws IllegalArgumentException{
+        boolean found = false;
+        for ( Player key : teamsMap.keySet() ) {
+            Player player = key;
+            if (player.getID() == playerID){
+                found = true;
+                System.out.println("Found Player\t" + "Player ID: "+ player.getID() +", Player Name: " + player.getName()+", Player Schedule: "+ player.getSchedule());
+            }
+            else{
+                found = false;
+                throw new IllegalArgumentException("Player Not Found");
+            }
+        }
+        return found;
+    }
+
 
     public String getTeamName(){
         return teamName;
     }
 
-    public String getTeam(){
-        return teamPlayers.toString();
+    public boolean getTeamInfo()throws IllegalArgumentException{
+        boolean found = false;
+        if (teamsMap.isEmpty()){
+            found = false;
+            throw new IllegalArgumentException("Team Not Found");
+        }
+        for ( Player key : teamsMap.keySet() ) {
+            found = true;
+            Player player = key;
+            System.out.println("Team: "+getTeamName()+ "\n");
+            System.out.println("Player ID: "+ player.getID() +", Player Name: " + player.getName()+", Player Schedule: "+ player.getSchedule().toString() + "\n");
+        }
+        return found;
     }
 
-    public ArrayList <Pair<Double, Double>> getTeamSchedule(){
-        return teamSchedule;
+    public void getTeamSchedule(){
+        Set set = teamsMap.entrySet();
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext()) {
+            Map.Entry item = (Map.Entry)iterator.next();
+            Player player = (Player) item.getKey();
+            System.out.println ("Player: "+ player.getName() +", Schedule: "+ item.getValue());
+        }
     }
-
-    public Pair<Double, Double> viewSchedule (Player player){
-        return player.getSchedule();
-    }
-
-
-
 }
--- src/test/java/edu/ithaca/dragon/bank/TeamTest.java
+++ src/test/java/edu/ithaca/dragon/bank/TeamTest.java
@@ -2,7 +2,9 @@ package edu.ithaca.dragon.bank;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.text.BreakIterator;
 
@@ -10,15 +12,15 @@ import org.junit.jupiter.api.Test;
 
 
 public class TeamTest {
-    Team createTeamsTest(String name){
+    Team createTeamForTest(String name){
         return new Team (name);
     }
     @Test
     void getTeamNameTest(){
-        Team team1 = createTeamsTest("Team 1");
-        Team team2 = createTeamsTest("Team2");
-        Team team3 = createTeamsTest("");
-        Team team4 = createTeamsTest(" ");
+        Team team1 = createTeamForTest("Team 1");
+        Team team2 = createTeamForTest("Team2");
+        Team team3 = createTeamForTest("");
+        Team team4 = createTeamForTest(" ");
 
         //Unit Test for team names
         assertEquals("Team 1", team1.getTeamName());
@@ -27,25 +29,39 @@ public class TeamTest {
         assertEquals("Invalid Team Name", team4.getTeamName());
     }
     @Test
-    //Integral Test for getting Player members (objects)
+    //Integral Test for getting Player Member objects
     void getTeamMembersTest(){
-        Team team1 = createTeamsTest("Team Test");
+        //Invalid Team
+        Team team1 = createTeamForTest("Invalid Team Test");
+        //Valid Team
+        Team team2 = createTeamForTest("Valid Team Test");
+        
+        //Add To Team Tests
+        assertFalse(team1.getTeamInfo());
+        
+        team2.addPlayer(new Player("Adam", "0000", 10.0, 11.0));
+        System.out.println(team2.getTeamName());
+        assertEquals("Player ID: "+ "0000" +"Player Name: " +"Adam"+"Player Schedule: "+ "10,0, 11.0" + "\n", team1.getTeamInfo());
+        assertTrue(team1.getTeamInfo());
 
-        team1.addPlayer(new Player("Adam", "0000", 10.0, 11.0));
-        assertEquals("Player 1: Adam, 0000, 10.0, 11.0", team1.getTeam());
+        team2.addPlayer(new Player("Amber", "0001", 9.0, 15.0));
+        team2.addPlayer(new Player("Bria", "0002", 18.0, 23.0));
+        team2.addPlayer(new Player("Eli", "0003", 5.0, 20.0));
+        team2.addPlayer(new Player("Nusi","0004", 13.0, 3.3));
 
+        assertEquals("Player ID: "+ "0001" +"Player Name: " +"Amber"+"Player Schedule: "+ "9,0, 15.0"+"\n" +
+        "Player ID: "+ "0002" +"Player Name: " +"Bria"+"Player Schedule: "+ "18.0,0, 23.0"+"\n"+
+        "Player ID: "+ "0003" +"Player Name: " +"Eli"+"Player Schedule: "+ "5.00, 20.0"+"\n"+
+        "Player ID: "+ "0004" +"Player Name: " +"Nusi"+"Player Schedule: "+ "13.0, 3.0"+"\n", team1.getTeamInfo());
 
-        team1.addPlayer(new Player("Amber", "0001", 9.0, 15.0));
-        team1.addPlayer(new Player("Bria", "0002", 18.0, 23.0));
-        team1.addPlayer(new Player("Eli", "0003", 5.0, 20.0));
-        team1.addPlayer(new Player("Nusi","0004", 13.0, 3.3));
+        
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {team2.addPlayer(new Player("Extra","0005", 1.0, 25.3));});
+        assertEquals("Cannot add player with invalid credentials.", exception.getMessage());
+        
+        //Remove From Team Tests
+        Throwable exception = assertThrows(IndexOutOfBoundsException.class, () -> {team1.removePlayer("0000");});
+        assertEquals("This team list is empty. There are no players in this team.", exception.getMessage());
 
-        assertEquals("Player 1: Adam, 0000, 10.0, 11.0; Player 2: Amber, 0001, 9.0, 15.0; Player 3: Bria, 0002, 18.0, 23.0; Player 4: Eli, 0003, 5.0, 20.0; Player 5: Nusi,0004, 13.0, 3.3; Player 6: Extra,0005, 1.0, 1.3", team1.getTeam());
- + 
-        team1.addPlayer(new Player("Extra","0005", 1.0, 1.3));
-        assertEquals("Player 1: Adam, 0000, 10.0, 11.0; Player 2: Amber, 0001, 9.0, 15.0; Player 3: Bria, 0002, 18.0, 23.0; Player 4: Eli, 0003, 5.0, 20.0; Player 5: Nusi,0004, 13.0, 3.3; Player 6: Extra,0005, 1.0, 1.3", team1.getTeam());
-
-        team1.removePlayer(team1.get);
     }
 
     @Test
