6bdffe7b7b244a167f8719aa6863dda1630b6746
NusiratO <nolumegbon@ithaca.edu>
2021-04-19 00:54:07 -0400

    Just started the ParentUI, however it's not running properly. Still working on it.

---
+++ src/main/java/edu/ithaca/dragon/bank/ParentUI.java
@@ -0,0 +1,31 @@
+package edu.ithaca.dragon.bank;
+
+import java.util.Scanner;
+
+public class ParentUI {
+    public static void main(String args[]) {
+        Scanner option = new Scanner(System.in);
+
+        System.out.println("Select an option or hit zero: ");
+        System.out.println("1. Create a new player");
+        System.out.println("2. Create a new team");
+        System.out.println("3. Create a new tournament");
+        int userOption = option.nextInt();
+
+        while(userOption >= 1 && userOption <= 3){
+            if( userOption == 1){
+                PlayerUI newPlayerUI = new PlayerUI();
+
+                System.out.println("Select an option or hit zero: ");
+                System.out.println("1. Create a new player");
+                System.out.println("2. Create a new team");
+                System.out.println("3. Create a new tournament");
+                userOption = option.nextInt();
+            }
+
+            else if(userOption == 2){
+
+            }
+        }
+    }
+    }
