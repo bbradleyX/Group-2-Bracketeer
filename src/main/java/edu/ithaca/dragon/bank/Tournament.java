5a95115ebc50ffe0feb27f8ad6e01963f45b4af3
Eli Robinson <elimrobinson@gmail.com>
2021-04-21 18:51:32 -0400

    Create Round and Update round Completed. Create Bracket Completed

--- src/main/java/edu/ithaca/dragon/bank/Tournament.java
+++ src/main/java/edu/ithaca/dragon/bank/Tournament.java
@@ -1,31 +1,44 @@
 package edu.ithaca.dragon.bank;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.io.*;
+
 //import jdk.internal.net.http.common.Pair;
 import org.javatuples.Pair;
+
+
 
 public class Tournament {
     private String name;
     private String ID;
     //private Team[] teams;
-    private Player[] players;
-    private Player[] inGame;
-    private Player[] elim;
+    private List<Player> players;
+    private List<Player> inGame;
+    private List<Player> elim;
     private double startTime;
+    private int group;
+    private List<Game> games;
 
     /**
      * Constructor for the Player class
      * @param name must be a name consisting of at least 1 character, number, or symbol
      * @param ID must be a string of 4 numbers
      * @param startTime must be a double greater than 0 and less than 24, using increments of 0.25 for 15 minute blocks of time
-     * @param players is a list of players in the teams participating in the event
+     * @param players2 is a list of players in the teams participating in the event
      * @throws IllegalArgumentException for any arguments that violate the above rules
      */
 
     //Constructor - Incomplete as of now using players to fill in for teams until we have that functionality
-    public Tournament(String name, String ID, double startTime,Player[] players) throws IllegalArgumentException {
+    public Tournament(String name, String ID, double startTime,List<Player> players2) throws IllegalArgumentException {
         this.name = name;
         this.ID = ID;
         this.startTime = startTime;
-        this.players = players;
+        this.players = players2;
+        this.group = 0;
+        this.games = new ArrayList<>();
+        this.inGame = new ArrayList<>();
+        this.elim = new ArrayList<>();
         ;
     }
 
@@ -45,27 +58,86 @@ public class Tournament {
         return this.teams;
     } */
 
-    public Player[] getPlayers(){
+    public List<Player> getPlayers(){
         return this.players;
+    }
+    
+    public List<Game> getGames(){
+        return this.games;
     }
 
     public boolean isRoundComplete(){
-        return false;
+        if(games.isEmpty()){
+            return true;
+        }
+        else{return false;
+        }
+    }
+
+    public void updateRound(){
+        int size = games.size();
+        List<Game> copy = new ArrayList<>();
+        for(int i=0;i<size;i++){
+            if(games.get(i).getWinner()!=null){
+                players.add(games.get(i).getWinner());
+                elim.add(games.get(i).getLoser());
+            }
+            else{
+                copy.add(games.get(i));
+            }
+        }
+        games = copy;
     }
 
     public boolean checkTournyFin(){
-        return false;
+        if(players.size()<2){
+            return true;
+        }
+        else{return false;
+        }
     }
 
     public void createRound(){
-       ;
+        if(isRoundComplete()){
+            if(!checkTournyFin()){
+                int i = 1;
+                int index1 = 0;
+                int index2 = 0;
+                int num = 0;
+                while(players.size()>1){
+                    Random rand = new Random();
+                    if(players.size()-2==0){
+                        num = 1;
+                    }
+                    else{
+                        num = rand.nextInt(players.size()-2)+1;
+                    }
+                    games.add(new Game("game" + i, "123"+i, startTime, players.get(0),players.get(num)));
+                    List<Player> copy = new ArrayList<>();
+                    index2 = num;
+                    for (int j = 0; j < players.size(); j++) {
+                        if (j != index1 && j != index2) {
+                        copy.add(players.get(j));
+                        }
+                        else{
+                            inGame.add(players.get(j));
+                        }
+                    }
+                    players = copy;
+                    i++;
+                    if(i>10){
+                        players.clear();
+                    }
+                }
+            }
+        }
     }
 
-    public Player[] getInGame(){
+    public List<Player> getInGame(){
         return this.inGame;
     }
 
-    public Player[] getElim(){
+    public List<Player> getElim(){
         return this.elim;
     }
     //This will need more methods but non are on the diagram as of right now.
--- src/test/java/edu/ithaca/dragon/bank/TournamentTest.java
+++ src/test/java/edu/ithaca/dragon/bank/TournamentTest.java
@@ -27,40 +27,115 @@ class TournamentTest {
         }
     
         @Test
-        void getNameTest() {
+        void constructorTest() {
             List<Player> players = new ArrayList<>();
-            players.add(new Player("A", "1234", 9.0, 5.5));
-
+            players.add(new Player("1", "1234", 9.0, 17.5));
+            players.add(new Player("2", "1235", 9.0, 17.5));
+            players.add(new Player("3", "1236", 9.0, 17.5));
+            players.add(new Player("4", "1237", 9.0, 17.5));
+            players.add(new Player("5", "1238", 9.0, 17.5));
+            Tournament tournament = new Tournament("tourny1", "1234", 9.0, players);
+            assertEquals("tourny1", tournament.getName());
+            assertEquals("1234", tournament.getID());
+            assertEquals(5, tournament.getPlayers().size());
         }
     
-        @Test
-        void getIDTest() {
-            Player myPlayer = new Player("Adam", "1234", 9.0, 5.5);
-            assertEquals(myPlayer.getID(), "1234");
-        }
-    
-        @Test
-        void getScheduleTest() {
-            Player myPlayer = new Player("Adam", "1234", 9.0, 17.5);
-            //assertEquals(myPlayer.getSchedule().getKey(), 9.0);
-            //assertEquals(myPlayer.getSchedule().getValue(), 17.5);
-        }
-
         @Test
         void isRoundCompleteTest () {
-            Player myPlayer = new Player("Adam", "1234", 9.0, 17.5);
-            //assertEquals(myPlayer.getSchedule().getKey(), 9.0);
-            //assertEquals(myPlayer.getSchedule().getValue(), 17.5);
+            List<Player> players = new ArrayList<>();
+            players.add(new Player("1", "1234", 9.0, 17.5));
+            players.add(new Player("2", "1235", 9.0, 17.5));
+            players.add(new Player("3", "1236", 9.0, 17.5));
+            players.add(new Player("4", "1237", 9.0, 17.5));
+            players.add(new Player("5", "1238", 9.0, 17.5));
+            Tournament tournament = new Tournament("tourny1", "1234", 9.0, players);
+            assertTrue(tournament.isRoundComplete());
+            tournament.createRound();
+            assertFalse(tournament.isRoundComplete());
+            
         }
 
         @Test
-        void checkTournyFin() {
-            Player myPlayer = new Player("Adam", "1234", 9.0, 17.5);
-            //assertEquals(myPlayer.getSchedule().getKey(), 9.0);
-            //assertEquals(myPlayer.getSchedule().getValue(), 17.5);
+        void checkTournyFinTest() {
+            List<Player> players3 = new ArrayList<>();
+            Tournament tournament3 = new Tournament("tourny1", "1234", 9.0, players3);
+            assertTrue(tournament3.checkTournyFin());
+            players3.add(new Player("1", "1234", 9.0, 17.5));
+            players3.add(new Player("2", "1235", 9.0, 17.5));
+            assertFalse(tournament3.checkTournyFin());
         }
-    
 
+        @Test
+        void createRoundTest() {
+            List<Player> players = new ArrayList<>();
+            players.add(new Player("1", "1234", 9.0, 17.5));
+            players.add(new Player("2", "1235", 9.0, 17.5));
+            players.add(new Player("3", "1236", 9.0, 17.5));
+            players.add(new Player("4", "1237", 9.0, 17.5));
+            players.add(new Player("5", "1238", 9.0, 17.5));
+            Tournament tournament = new Tournament("tourny1", "1234", 9.0, players);
+            tournament.createRound();
+            assertEquals(1,tournament.getPlayers().size());
+            assertEquals(4,tournament.getInGame().size());
+            assertEquals(2,tournament.getGames().size());
 
+            List<Player> players2 = new ArrayList<>();
+            players2.add(new Player("1", "1234", 9.0, 17.5));
+            Tournament tournament2 = new Tournament("tourny1", "1234", 9.0, players2);
+            tournament2.createRound();
+            assertEquals(1,tournament2.getPlayers().size());
+            assertEquals(0,tournament2.getInGame().size());
+            assertEquals(0,tournament2.getGames().size());
+
+            List<Player> players3 = new ArrayList<>();
+            Tournament tournament3 = new Tournament("tourny1", "1234", 9.0, players3);
+            tournament3.createRound();
+            assertEquals(0,tournament3.getPlayers().size());
+            assertEquals(0,tournament3.getInGame().size());
+            assertEquals(0,tournament3.getGames().size());
+        }
+
+        @Test
+        void updateRoundTest() {
+            List<Player> players = new ArrayList<>();
+            players.add(new Player("1", "1234", 9.0, 17.5));
+            players.add(new Player("2", "1235", 9.0, 17.5));
+            players.add(new Player("3", "1236", 9.0, 17.5));
+            players.add(new Player("4", "1237", 9.0, 17.5));
+            players.add(new Player("5", "1238", 9.0, 17.5));
+            Tournament tournament = new Tournament("tourny1", "1234", 9.0, players);
+            //creates round 1 - 2 games of 2 and 1 bye
+            tournament.createRound();
+            // completes both games
+            tournament.getGames().get(0).hasWon(tournament.getGames().get(0).getPlayer1());
+            tournament.getGames().get(1).hasWon(tournament.getGames().get(1).getPlayer1());
+            // updated - 0 games 2 elim 3 players
+            assertEquals(2,tournament.getGames().size());
+            tournament.updateRound();
+            assertEquals(0,tournament.getGames().size());
+            assertEquals(2,tournament.getElim().size());
+            assertEquals(3,tournament.getPlayers().size());
+            // creates round 2 - 1 game of 2 1 bye
+            tournament.createRound();
+            //completes game
+            tournament.getGames().get(0).hasWon(tournament.getGames().get(0).getPlayer1());
+            //updated - 0 games 3 elim 2 players
+            tournament.updateRound();
+            assertEquals(0,tournament.getGames().size());
+            assertEquals(3,tournament.getElim().size());
+            assertEquals(2,tournament.getPlayers().size());
+            //creates round 3 - 1 game of 2
+            tournament.createRound();
+            assertEquals(0,tournament.getPlayers().size());
+            assertEquals(1,tournament.getGames().size());
+            //completes game
+            tournament.getGames().get(0).hasWon(tournament.getGames().get(0).getPlayer1());
+            //updated 0 games 4 elim 1 player(winner)
+            tournament.updateRound();
+            assertTrue(tournament.checkTournyFin());
+            assertEquals(1,tournament.getPlayers().size());
+            assertEquals(4,tournament.getElim().size());
+
+        }
     }
     //@Test
