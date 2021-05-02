9bdf96ef6e2fec8673cc70f11ce5be2f53166b33
Adam Marciszewski <amarciszewski@ithaca.edu>
2021-05-02 14:07:31 -0400

    Updated Tournament constructor to hold Game

--- src/main/java/edu/ithaca/dragon/bank/Tournament.java
+++ src/main/java/edu/ithaca/dragon/bank/Tournament.java
@@ -2,31 +2,32 @@ package edu.ithaca.dragon.bank;
 import java.util.ArrayList;
 
 //import jdk.internal.net.http.common.Pair;
-import org.javatuples.Pair;
+
 
 public class Tournament {
     private String name;
     private String ID;
-    //private Team[] teams;
-    private ArrayList<Team> players;
+    private ArrayList<Team> teams;
     private double startTime;
+    private Game gameType;
 
     /**
      * Constructor for the Player class
      * @param name must be a name consisting of at least 1 character, number, or symbol
      * @param ID must be a string of 4 numbers
+     * @param gameType an object of Game that holds information on the specific game being played
      * @param startTime must be a double greater than 0 and less than 24, using increments of 0.25 for 15 minute blocks of time
-     * @param players is a list of players in the teams participating in the event
+     * @param teams is a list of teams in the teams participating in the event
      * @throws IllegalArgumentException for any arguments that violate the above rules
      */
 
     //Constructor
-    public Tournament(String name, String ID, double startTime, ArrayList<Team> players) throws IllegalArgumentException {
+    public Tournament(String name, String ID, Game gameType, double startTime, ArrayList<Team> teams) throws IllegalArgumentException {
         this.name = name;
         this.ID = ID;
+        this.gameType = gameType;
+        this.teams = teams;
         this.startTime = startTime;
-        this.players = players;
-        ;
     }
 
     public static boolean validateArguments(String name, String ID, double startTime) {
@@ -61,12 +62,16 @@ public class Tournament {
         return this.ID;
     }
 
-    /* public Team[] getTeams() {
+    public ArrayList<Team> getTeams(){
         return this.teams;
-    } */
+    }
 
-    public ArrayList<Team> getPlayers(){
-        return this.players;
+    public double getStartTime(){
+        return this.startTime;
+    }
+
+    public Game getGameType(){
+        return this.gameType;
     }
 
     //This will need more methods but non are on the diagram as of right now.
