99e5a4914ab19e2fe4ecdda48d73337f35da137d
aelliott <aelliott@ithaca.edu>
2021-05-06 20:29:02 -0400

    Small fixes

--- src/test/java/edu/ithaca/dragon/bank/PlayerMakerUITest.java
+++ src/test/java/edu/ithaca/dragon/bank/PlayerMakerUITest.java
@@ -6,15 +6,15 @@ import java.util.Scanner;
  /*
     -----------------------------------Making a Player--------------------------------------------
     
-    For your name: there are no Restrictions 
-    For ID: try entering a letter or less than four number. You should received a message saying please reenter.
-    For startTime: try enter a number greater than 24 or a letter or negative number or zero. You should received a message saying please reenter.
-    For endTime: try enter a number greater than 24 or a letter or negative number or zero. You should received a message saying please reenter.
+    For your name: there are no Restrictions, enter anyname with Symbols.
+    For ID: try entering a letter or less than four number. You should received a message saying please reenter. Enter an ID with four number, like 1234.
+    For startTime: try enter a number greater than 24 or a letter or negative number or zero. You should received a message saying please reenter. Enter a start time between the restrictions mentioned, like 8 or 2.23 or 4.0 
+    For endTime: try enter a number greater than 24 or a letter or negative number or zero. You should received a message saying please reenter. Enter a end time between the restrictions mentioned, like 6 or 5.03 or 7.2 
     
     -----------------------------------Modify Player Schedule --------------------------------------------
     Asking you if you want to modify Schedule If you don't enter yes or no You should received a message saying please reenter.
-    For startTime: try enter a number greater than 24 or a letter or negative number or zero. You should received a message saying please reenter.
-    For endTime: try enter a number greater than 24 or a letter or negative number or zero. You should received a message saying please reenter.
+    For startTime: try enter a number greater than 24 or a letter or negative number or zero. You should received a message saying please reenter. Enter a start time between the restrictions mentioned, like 20 or 1.11 or 4.0
+    For endTime: try enter a number greater than 24 or a letter or negative number or zero. You should received a message saying please reenter. Enter a end time between the restrictions mentioned, like 23 or 10.21 or 12.4 
 
     */
 class PlayerMakerUITest extends PlayerMakerUI{
